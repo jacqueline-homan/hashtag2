5f592b0bf5a79cf45bc6506813f8dff7a9c47656
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-01-03 17:15:34 -0500

    added rspec, capybara, and simplecov gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtag2::Application.config.secret_token = '89dbf7a1172e5721aea905497c4cb1a10d673495fe3551525d8131d6e7c3224471a9e954e75750191cad28ccd21dce11f95f383446ab379c7f71dd8b83611526'
