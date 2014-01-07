d61fbb8caad5064c60b7dc91b9daae69316311e5
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-01-07 11:40:39 -0500

    fixed code in initializers/twitter.rb file

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |twitter|
-  twitter.consumer_key = ENV['TFWS_KEY']
-  twitter.consumer_secret = ENV['TFWS_SECRET']
+  twitter.consumer_key = ENV['TWITTER_KEY']
+  twitter.consumer_secret = ENV['TWITTER_SECRET']
   twitter.access_token = ENV['TWITTER_ACCESS_TOKEN']
   twitter.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
